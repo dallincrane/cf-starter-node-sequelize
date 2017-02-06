0f4b6937568e0b8bfe923778a379fe0447cb8f12
Dallin Crane <dallincrane@gmail.com>
2017-02-05 18:33:27 -0600

    update postgres service name

--- server/config/postgres.js
+++ server/config/postgres.js
@@ -23,13 +23,13 @@ config.production = {}
 
 if (process.env.VCAP_APPLICATION) {
   const appEnv = cfenv.getAppEnv()
-  const connUri = appEnv.getService('cf-spring-db').credentials.uri
+  const connUri = appEnv.getService('postgres-core').credentials.uri
   const connUriParts = url.parse(connUri)
-  
+
   config.production.host = connUriParts.hostname
   config.production.port = connUriParts.port
   config.production.database = connUriParts.pathname.substring(1)
-  
+
   const uriAuth = connUriParts.auth.split(':')
   config.production.username = uriAuth[0]
   config.production.password = uriAuth[1]
